5b8673c7d5a1b615652ed4b618c0b1051eaf8206
noman <nomanshabeer27@gmail.com>
2026-02-23 12:02:14 +0500

    add private_subnet

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ variable "vpc_name" {
 variable "public_subnet_cidr" {
   type = string
 }
+variable "private_subnet_cidr" {
+  type = string
+}
 variable "az" {
   type = string
 }
@@ -22,15 +25,22 @@ resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.public_subnet_cidr
   availability_zone = var.az
-
   tags = {
     Name = "${var.vpc_name}-public-subnet"
   }
 }
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az
+  tags = {
+    Name = "${var.vpc_name}-private-subnet"
+  }
+}
+
 # Internet Gateway
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
-
   tags = {
     Name = "${var.vpc_name}-internet-gateway"
   }
@@ -44,12 +54,13 @@ resource "aws_route_table" "public_rt" {
 }
 # Route to Internet Gateway
 resource "aws_route" "public_internet_access" {
-    route_table_id         = aws_route_table.public_rt.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
 }
 # Associate public subnet with route table
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+
--- output.tf
+++ output.tf
@@ -4,3 +4,6 @@ output "vpc_id" {
 output "public_subnet_id" {
   value = aws_subnet.public_subnet.id
 }
+output "private_sunbnet_id" {
+  value = aws_subnet.private_subnet.id
+}
